41318451feb945cdda7fe2e854b8791a185e4377
roger <roger@policiacivil.pe.gov.br>
2024-11-22 10:50:58 -0300

    primeiro commit

---
+++ src/developers/developers.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { CreateDeveloperDto } from './dto/create-developer.dto';
+import { UpdateDeveloperDto } from './dto/update-developer.dto';
+import { Repository } from 'typeorm';
+import { Developer } from './entities/developer.entity';
+
+@Injectable()
+export class DevelopersService {
+  constructor(private readonly repository: Repository<Developer>) { }
+
+  create(dto: CreateDeveloperDto) {
+    const developer = this.repository.create(dto);
+    return this.repository.save(developer);
+  }
+
+  findAll() {
+    return this.repository.find();
+  }
+
+  findOne(id: string) {
+    return this.repository.findOneBy({ id });
+  }
+
+  async update(id: string, dto: UpdateDeveloperDto) {
+    const developer = await this.repository.findOneBy({ id });
+    if (!developer) return null;
+    this.repository.merge(developer, dto);
+    return this.repository.save(developer);
+  }
+
+  async remove(id: string) {
+    const developer = await this.repository.findOneBy({ id });
+    if (!developer) return null;
+    return this.repository.remove(developer);
+  }
+}
